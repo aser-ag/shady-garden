687aef27a4821311483f4dfe75d3f39a80e0e0a4
Aser <aseralemu03@gmail.com>
2026-05-17 00:39:44 +0300

    feat: implement user registration and JWT authentication

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +29,22 @@ const userSchema = mongoose.Schema({
     timestamps: true
 
 });
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
 
 const User = mongoose.model('User', userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
+//import
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
-dotenv.config()
+dotenv.config();
 await connectDB();
 
 const app = express();
@@ -12,16 +13,6 @@ app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5173'
 }));
-
-app.get('/test', (req, res) => {
-    res.send('server is working');
-});
-
-app.get('/api/test', (req, res) => {
-    res.json({
-        status: 'success', message: 'API is running'
-    });
-});
 
 app.listen(5000, () => {
     console.log('server running on http://localhost:5000');
